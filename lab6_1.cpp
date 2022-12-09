ad0b973505f6e2cc34ac0dafe0b7fdf366250848
PremerX007 <bannawat_v@cmu.ac.th>
2022-12-09 15:17:12 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int ip=1,even=0,odd=0;
+    while (true){
+        cout << "Enter an integer: ";
+        cin >> ip;
+
+        if (ip==0){
+            break;
+        }
+
+        if (ip%2==0){
+            even += 1;
+        }else{
+            odd += 1;
+        }
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
